ce8b3eb85c3dc01dcb7bba4e7eddd641d2d3fa1e
sandrokhmiadashvili <khmiadashvili.sandro@bk.ru>
2025-10-31 21:56:48 +0400

    second commit

---
+++ day02/homework/hw2.py
@@ -0,0 +1,9 @@
+#2) შექმენით სამი ცვლადი სადაც შეინახავთ ყველა მონაცემთა ტიპის მაგალითს
+name = "sandro"
+print(name)
+
+number = 5
+print(number)
+
+numberfloat = 6.43
+print(numberfloat)
--- day02/homework/hw3.py
+++ day02/homework/hw3.py
@@ -1,6 +1,21 @@
 #3) დაპრინტე ყველა მათემატიკური ოპერატორის და გვერდზე კომენტარის სახით მიუწერე რას გამოიტანს
 
-#+ - გამოიტანს მიმატებას
-#-   გამოიტანს გამოკლებას
-#*   გამოიტანს გამრავლებას
-#/   გამოიტანს გაყოფას
+mimateba = 65+43
+print(mimateba)
+
+gamokleba = 54-23
+print(gamokleba)
+
+gamravleba = 43*32
+print(gamravleba)
+
+gayofa = 54/2
+print(gayofa)
+
+unashtogayofa = 54//2
+print(unashtogayofa)
+
+mtelinishani = 43**34
+print(mtelinishani)
+
+
