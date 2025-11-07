e4abcdd52dbeb31bc356db28ef5022a801a57d85
sandrokhmiadashvili <khmiadashvili.sandro@bk.ru>
2025-11-07 17:41:37 +0400

    second commit

---
+++ day05/classwork/homework/cw4.py
@@ -0,0 +1,13 @@
+# გაასწორე შემდეგი კოდები:
+#name = "saba"
+#name = "giorgi"
+#print(Name)
+#num = "40"
+#print(num * "3")
+
+name = "saba"
+NAME = "giorgi"
+print(NAME)
+
+num = 40 
+print(num * 3)
