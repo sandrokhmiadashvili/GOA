c41233d62e7442fdabe88057da97f5648197d616
sandrokhmiadashvili <khmiadashvili.sandro@bk.ru>
2025-11-28 18:01:12 +0400

    second commit

---
+++ day07/homework/hw5.py
@@ -0,0 +1,16 @@
+operatiors = input("შეიყვანეთ ოპერატიული (+,-,*,/)")
+Num1 = float(input("შეიყვანეთ ახლანდელი წელი:" ))
+Num2 = float(input("შეიყვანეთ დაბადების წელი:" ))
+
+
+if operatiors == "+":
+    print(Num1 + Num2)
+
+elif operatiors == "-":
+    print(Num1 - Num2)
+
+elif operatiors =="/":
+    print(Num1 / Num2)
+
+elif operatiors =="*":
+    print(Num1 * Num2)
